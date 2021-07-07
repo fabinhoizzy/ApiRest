85e7654f0d556e2e14a23d5b0de5877c488896b5
fabinhoizzy <fabinhoevil16@hotmail.com>
2021-07-06 21:32:57 -0300

    Criando a estrutura do projeto

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+//api/users/1
+if($_GET['url']) {
+    $url = explode('/', $_GET['url']);
+
+    if($url[0] === 'api') {
+        //..
+    }
+}
+
+
